6cc51d26f366b4230a04adcd9640d93f29054e0f
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2018-10-24 13:17:40 +0800

    增加任務 error

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2018/10/24
- * Time: 下午 01:07
- */
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+        <br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
